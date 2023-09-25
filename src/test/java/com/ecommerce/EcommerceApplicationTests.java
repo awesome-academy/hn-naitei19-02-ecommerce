9a24b6714e0c08c6877f4636673c7870e5f39543
sonbk040901 <leducson007@gmail.com>
2023-09-25 18:25:12 +0700

    Hoàn thiện phần giao diện

--- src/test/java/com/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/ecommerce/EcommerceApplicationTests.java
@@ -1,13 +1,21 @@
 package com.ecommerce;
 
+import com.ecommerce.dao.CartDAO;
+import com.ecommerce.model.Cart;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest(classes= EcommerceApplicationTests.class)
 class EcommerceApplicationTests {
-
+	@Autowired
+	private CartDAO cartDAO;
 	@Test
-	void contextLoads() {
+	void test() {
+		Optional<Cart> cart = cartDAO.findByUserId(9L);
+		System.out.println(cart.orElseThrow().getCartDetails().size());
 	}
 
 }
